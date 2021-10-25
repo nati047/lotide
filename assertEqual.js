d72f0b12bcbc4d2adc646aaa764d38c22f9c4b28
Natnael Tekletsadik <isaacnatnael@gmail.com>
2021-10-25 21:59:02 +0000

    with back ticks

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('🟢🟢🟢Assertion Passed: ' + actual + ' === ' + expected);
+    console.log(`🟢🟢🟢Assertion Passed: ${actual} === ${expected}`);
   }
   if (actual !== expected) {
-    console.log('🔴🔴🔴Assertion Failed: ' + actual + ' !== ' + expected);
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
