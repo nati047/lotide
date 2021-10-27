e9de2f84a9f43c4bb2f9a9059e9f692c5ea38689
Natnael Tekletsadik <isaacnatnael@gmail.com>
2021-10-27 22:20:50 +0000

    impliment letterPosition

---
+++ letterPositions.js
@@ -0,0 +1,29 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) return false;
+  else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) return false;
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) console.log(`✅✅✅ Assertion Passed: ${array1} is equal to ${array2}`);
+
+  else console.log(`❌❌❌ Assertion Failed: ${array1} is not equal to ${array2}`);
+};
+
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] === ' ') continue;
+    else if (!results[sentence[i]]) {
+      results[sentence[i]] = [];
+      results[sentence[i]].push(i);
+    } else
+      results[sentence[i]].push(i);
+  }
+  return results;
+};
+
