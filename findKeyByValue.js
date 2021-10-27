ee988d6247cd117cc9e0f04ad70333537147dadd
Natnael Tekletsadik <isaacnatnael@gmail.com>
2021-10-27 22:36:32 +0000

    impliment findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢Assertion Passed: ${actual} === ${expected}`);
+  }
+  if (actual !== expected) {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  for (let key in object) {
+    if (object[key] === value) return key;
+  }
+};
+
